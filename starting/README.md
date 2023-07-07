69c724d236ff05bd6120a3d3cccaa7827b4920fa
adrian <adrian.lpes@gmail.com>
2023-07-07 18:26:18 +0200

    fix: wipe

---
+++ starting/README.md
@@ -0,0 +1,13 @@
+# AUTOMATING INITIAL SETUP
+
+This image is intended for reproduce a empty state and testing initializer script.
+
+# HOW TO USE
+
+Build the image with docker
+
+```bash
+docker-compose run -rm init
+```
+
+For cleanup every process is kill
--- starting/init.sh
+++ starting/init.sh
@@ -9,16 +9,17 @@ NC=$(tput sgr0)
 BOLD=$(tput bold)
 
 function install_brew () {
+  echo "Installing homebrew"
   if command -v brew >/dev/null; then
+    echo "Already installed"
     brew --version
   else
     git clone https://github.com/Homebrew/brew ~/.linuxbrew/Homebrew \
       && mkdir ~/.linuxbrew/bin \
       && ln -s ../Homebrew/bin/brew ~/.linuxbrew/bin \
-      && eval $(~/.linuxbrew/bin/brew shellenv) \
       && brew --version
   fi
-
+  echo "Finished homebrew installation"
 }
 
 # MAIN
