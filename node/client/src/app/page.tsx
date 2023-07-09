633cd4e188386b3861d46825a11fba537394560a
adrian <adrian.lpes@gmail.com>
2023-07-09 20:55:03 +0200

    feat: connected to front

--- node/client/src/app/page.tsx
+++ node/client/src/app/page.tsx
@@ -1,14 +1,23 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 
-export default function Home() {
+
+export default async function Home() {
+  const data = await fetch(process.env.API_URL || '').then(data => data.json());
+
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
-          Get started by jarlinggggg editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
+        <div>
+                  {data.map(
+          ({ name, _id }: { _id: string, name: string }) => (
+            <p key={_id}>
+              Get started by {name} editing&nbsp;
+              <code className={styles.code}>src/app/page.tsx</code>
+            </p>
+          )
+        )}
+        </div>
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
