211cb2563a58813c9f30187e9fb7ce6fbd9d4614
adrian <adrian.lpes@gmail.com>
2023-07-19 18:14:46 +0200

    feat: added treesitter plugin

--- nvim/src/lua/plugins/protocol/init.lua
+++ nvim/src/lua/plugins/protocol/init.lua
@@ -3,9 +3,13 @@ local TABLES = require('utils.functions.tables')
 local MASON = require('plugins.protocol.mason')
 local LSP = require('plugins.protocol.lsp')
 local CMP = require('plugins.protocol.cmp')
+local TYPESCRIPT = require('plugins.protocol.typescript')
+local TREESITTER = require('plugins.protocol.treesitter')
 
 return TABLES.table_merge(
   LSP,
   MASON,
-  CMP
+  CMP,
+  TYPESCRIPT,
+  TREESITTER
 )
